6da7baa3e3a6f5e9c0470facee48c498b78278c4
LokilokeshM <78999166+LokilokeshM@users.noreply.github.com>
2021-10-15 21:29:28 +0530

    Revert "Icare reception"

--- projects/icare-main/src/app/reception/reception.module.ts
+++ projects/icare-main/src/app/reception/reception.module.ts
@@ -2,17 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReceptionRoutingModule } from './reception-routing.module';
-import { DashboardComponent } from './Components/dashboard/dashboard.component';
-import { PatientRegistrationComponent } from './Components/patient-registration/patient-registration.component';
-import { PatientSearchComponent } from './Components/patient-search/patient-search.component';
 
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    PatientRegistrationComponent,
-    PatientSearchComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     ReceptionRoutingModule
